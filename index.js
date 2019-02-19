2477af00cf5055d22402c3c3bb0c92d2244b826c
Gavrilov Dmitry <black.heart@inbox.ru>
2019-02-19 15:37:03 +0300

    Finish palindrome methode

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
+// Return only words
+String.prototype.lettersOnly = function() {
+  return this.split(/\W+/).join("");
+}
+
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.content.toLowerCase().lettersOnly();
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
